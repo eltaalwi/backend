d7df8d844b804cc2e9073c41c35ae5c1c7e4ca97
eltaalwi <136832633+eltaalwi@users.noreply.github.com>
2023-08-17 03:54:23 +0300

    df
    df

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // database
+  MONGODB_USERNAME: "",
+  MONGODB_PASSWORD: "",
+  MONGODB_DB_NAME: "",
+
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "",
+
+  //gmail credentials for send email
+  EMAIL: "",
+  PASSWORD: "",
+
+  //secret key for jwt
+  JWT_SECRET: "",
+
+  SERVER_PATH: "", // example :https://sdgserhgdhbrtsfgs.com/
+
+  // firebase server key for notification
+  SERVER_KEY: "",
+};
